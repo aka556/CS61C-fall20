a4dd62dd717849950a589a540d26972fc9aa9fd1
Dandelion-Free <167851871+aka556@users.noreply.github.com>
2025-09-18 00:40:38 +0800

    finished exercise3

--- Lab/lab03/factorial.s
+++ Lab/lab03/factorial.s
@@ -5,7 +5,7 @@ n: .word 8
 
 .text
 main:
-    la t0, n
+    la t0, n # t0是n的寄存器
     lw a0, 0(t0)
     jal ra, factorial
 
@@ -22,3 +22,25 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    # 基础情况, n = 0, n = 1
+    addi t0, x0, 1 # t0 = 1
+    beq a0, x0, return_one
+    beq a0, t0, return_one
+
+    addi sp, sp, -8 # 初始化寄存器，分配栈空间
+    sw ra, 0(sp) # 保存返回地址
+    sw a0, 4(sp) # 保存当前n的值
+
+    addi a0, a0, -1
+    jal ra, factorial # 递归调用
+
+    lw t1, 4(sp)
+    lw ra, 0(sp)
+    addi sp, sp, 8
+
+    mul a0, a0, t1
+    jr ra
+
+return_one:
+    addi a0, x0, 1
+    jr ra
